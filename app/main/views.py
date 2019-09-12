8cff73b755b05fa434fc963c43a3f4e8d197f64c
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-12 17:22:29 +0200

    views was updated

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,13 @@
-# from flask import render_template
-# from app import app
+from flask import render_template
+from app import app
 
-# # Views
-# @app.route('/')
-# def index():
+# Views
+@app.route('/')
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     title = 'Home - Welcome to The News Website Online'
-#     return render_template('index.html', title = title)
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Welcome to The News Website Online'
+    return render_template('index.html', title = title)
 
