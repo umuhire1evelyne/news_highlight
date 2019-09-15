614fe887acef765bb5520cbea08a94370d14e298
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-15 11:52:23 +0200

    config.py was updated

--- config.py
+++ config.py
@@ -1,27 +1,29 @@
-class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
+import os
 
+
+class Config:
+    """
+    General config
+    """
+    NEWS_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    SOURCES_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API')
+
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
