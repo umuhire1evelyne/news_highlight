f5cc374f2cd05ffee8a3294cf908cd825b1f2c3a
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-15 11:54:10 +0200

    manage.py was updated

---
+++ manage.py
@@ -0,0 +1,13 @@
+from app import create_app
+from flask_script import Manager, Shell, Server
+
+app = create_app('production')
+
+manager = Manager(app)
+
+manager.add_command('server', Server)
+
+
+if __name__ == '__main__':
+    manager.run()
+
