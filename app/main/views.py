f8a7fc4fea1b90d76e64d715381b0e051131a3c6
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-15 11:26:37 +0200

    views.py was updated

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,28 @@
 from flask import render_template
-from app import app
+from ..requests import get_news, get_sources
+from . import main
 
-# Views
-@app.route('/')
+
+
+@main.route('/')
 def index():
+    """
+    View root page
+    :return:
+    """
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    title = 'Home - Welcome to The News Website Online'
-    return render_template('index.html', title = title)
+    articles = get_news()
+    title = 'highlights - News app'
+    return render_template('index.html', title=title , articles = articles)
 
+
+@main.route('/sources')
+def source():
+    """
+    view sources page
+    :return:
+    """
+
+    sources = get_sources()
+    title = 'News sources - News app'
+    return render_template('sources.html', title = title, sources = sources)
